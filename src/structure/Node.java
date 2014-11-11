0f041fc00dbb91b6c5932a830bb9d3ac9750d05f
szym1000 <szym1000@gmail.com>
2014-11-11 14:31:54 +0100

    Abstract Node creation

--- src/structure/Node.java
+++ src/structure/Node.java
@@ -1,5 +1,75 @@
 package structure;
 
-public class Node {
+public abstract class Node {
+	
+	/**
+	 * Array holding the values of the demand for this node
+	 * for each day of the considered time period.
+	 */
+	private int[] demand;
+	
+	/**
+	 * Array holding the stock level for this node
+	 * for each day of the considered time period.
+	 */
+	private int[] stockLevel;
+	
+	/**
+	 * The searched optimal value of the stock level (yd).
+	 */
+	private int referenceStockLevel;
+	
+	/**
+	 * The cost of holding an item in the stock (pi h).
+	 */
+	private double holdingCost;
+	
+	/**
+	 * The cost of purchasing (delivery of?) an item (pi p).
+	 */
+	private double purchaseCost;
+	
+	
 
+	public int[] getDemand() {
+		return demand;
+	}
+
+	public void setDemand(int[] demand) {
+		this.demand = demand;
+	}
+
+	public int[] getStockLevel() {
+		return stockLevel;
+	}
+
+	public void setStockLevel(int[] stockLevel) {
+		this.stockLevel = stockLevel;
+	}
+
+	public int getReferenceStockLevel() {
+		return referenceStockLevel;
+	}
+
+	public void setReferenceStockLevel(int referenceStockLevel) {
+		this.referenceStockLevel = referenceStockLevel;
+	}
+
+	public double getHoldingCost() {
+		return holdingCost;
+	}
+
+	public void setHoldingCost(double holdingCost) {
+		this.holdingCost = holdingCost;
+	}
+
+	public double getPurchaseCost() {
+		return purchaseCost;
+	}
+
+	public void setPurchaseCost(double purchaseCost) {
+		this.purchaseCost = purchaseCost;
+	}
+	
+	
 }
